f58eeb9caceb9466904a3c01120cba3f2626e909
Danny Lockard <danny.lockard@banno.com>
2019-11-06 22:10:51 -0600

    Use eslintrc from nodecg

--- extension.js
+++ extension.js
@@ -1,71 +1,71 @@
-"use strict";
+'use strict';
 
-module.exports = function(nodecg) {
-  var donationsRep = nodecg.Replicant("donations", {
-    defaultValue: []
-  });
+module.exports = function (nodecg) {
+	const donationsRep = nodecg.Replicant('donations', {
+		defaultValue: []
+	});
 
-  var totalRep = nodecg.Replicant("total", {
-    defaultValue: 0
-  });
+	const totalRep = nodecg.Replicant('total', {
+		defaultValue: 0
+	});
 
-  if (nodecg.bundleConfig.justgiving_appid === "") {
-    nodecg.log.info(
-      "Please set justgiving_appid in cfg/nodecg-justgiving.json"
-    );
-    return;
-  }
+	if (nodecg.bundleConfig.justgiving_appid === '') {
+		nodecg.log.info(
+			'Please set justgiving_appid in cfg/nodecg-justgiving.json'
+		);
+		return;
+	}
 
-  if (nodecg.bundleConfig.justgiving_shorturl === "") {
-    nodecg.log.info(
-      "Please set justgiving_shorturl in cfg/nodecg-justgiving.json"
-    );
-    return;
-  }
+	if (nodecg.bundleConfig.justgiving_shorturl === '') {
+		nodecg.log.info(
+			'Please set justgiving_shorturl in cfg/nodecg-justgiving.json'
+		);
+		return;
+	}
 
-  const axios = require("axios");
-  const instance = axios.create({
-    baseURL: "https://api.justgiving.com",
-    timeout: 10000,
-    headers: { "Content-Type": "application/json" }
-  });
+	const axios = require('axios');
+	const instance = axios.create({
+		baseURL: 'https://api.justgiving.com',
+		timeout: 10000,
+		headers: {'Content-Type': 'application/json'}
+	});
 
-  async function askJustGivingForDonations() {
-    const response = await instance.get(
-      `/${nodecg.bundleConfig.justgiving_appid}/v1/fundraising/pages/${nodecg.bundleConfig.justgiving_shorturl}/donations`
-    );
+	async function askJustGivingForDonations() {
+		const response = await instance.get(
+			`/${nodecg.bundleConfig.justgiving_appid}/v1/fundraising/pages/${nodecg.bundleConfig.justgiving_shorturl}/donations`
+		);
 
-    const donationsData = response.data;
+		const donationsData = response.data;
 
-    for (let i = 0; i < donationsData.donations.length; i++) {
-      var found = donationsRep.value.find(function(element) {
-        return element.id === donationsData.donations[i].id;
-      });
-      if (found === undefined) {
-        donationsData.donations[i].shown = false;
-        donationsData.donations[i].read = false;
-        donationsRep.value.push(donationsData.donations[i]);
-      }
-    }
-  }
+		for (let i = 0; i < donationsData.donations.length; i++) {
+			const found = donationsRep.value.find(element => {
+				return element.id === donationsData.donations[i].id;
+			});
+			if (found === undefined) {
+				donationsData.donations[i].shown = false;
+				donationsData.donations[i].read = false;
+				donationsRep.value.push(donationsData.donations[i]);
+			}
+		}
+	}
 
-  async function askJustGivingForTotal() {
-    const response = await instance.get(
-      `/${nodecg.bundleConfig.justgiving_appid}/v1/fundraising/pages/${nodecg.bundleConfig.justgiving_shorturl}`
-    );
-    if (totalRep.value < response.data.totalRaisedOnline) {
-      totalRep.value = response.data.totalRaisedOnline;
-    }
-  }
+	async function askJustGivingForTotal() {
+		const response = await instance.get(
+			`/${nodecg.bundleConfig.justgiving_appid}/v1/fundraising/pages/${nodecg.bundleConfig.justgiving_shorturl}`
+		);
+		if (totalRep.value < response.data.totalRaisedOnline) {
+			totalRep.value = response.data.totalRaisedOnline;
+		}
+	}
 
-  function askJustGiving() {
-    askJustGivingForDonations();
-    askJustGivingForTotal();
-  }
+	function askJustGiving() {
+		askJustGivingForDonations();
+		askJustGivingForTotal();
+	}
 
-  setInterval(function() {
-    askJustGiving();
-  }, 5000);
+	setInterval(() => {
+		askJustGiving();
+	}, 5000);
 
-  askJustGiving();
+	askJustGiving();
 };
